eb9f516265198b207bb152250fc3e1c1cdf30253
Gerard Wagener <gerard.wagener@circl.lu>
2011-10-05 18:53:50 +0200

    Customized version of nfreader to index IP addresses
    This file should be copied in the bin directory of nfdump
    tool by Peter Haag and manually compiled as it is stated in
    the header.
    
    How the index works:
    
    In redis keys are created such as n4:10.0.0.1. Each key has a set
    as value. An IP address always starts with n followed by 4 (IPv4)
    or by 6 (IPv6). This prefix is followed by the string representation
    of the IP address. The associated set contains numbers such as 1,2,3...
    where each number is the index of a file name. A set can be
    queried in redis with the SMEMBERS command. The associated filename
    can be queried with the command GET d:x where x is the index (number)
    in the queried set.

---
+++ bin/nfreader.c
@@ -0,0 +1,533 @@
+/*
+ *  Copyright (c) 2009, Peter Haag
+ *  Copyright (c) 2004-2008, SWITCH - Teleinformatikdienste fuer Lehre und Forschung
+ *  All rights reserved.
+ *
+ *  Redistribution and use in source and binary forms, with or without
+ *  modification, are permitted provided that the following conditions are met:
+ *
+ *   * Redistributions of source code must retain the above copyright notice,
+ *     this list of conditions and the following disclaimer.
+ *   * Redistributions in binary form must reproduce the above copyright notice,
+ *     this list of conditions and the following disclaimer in the documentation
+ *     and/or other materials provided with the distribution.
+ *   * Neither the name of SWITCH nor the names of its contributors may be
+ *     used to endorse or promote products derived from this software without
+ *     specific prior written permission.
+ *
+ *  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ *  AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ *  IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ *  ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+ *  LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ *  CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ *  SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ *  INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ *  CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ *  ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ *  POSSIBILITY OF SUCH DAMAGE.
+ *
+ *  $Author: haag $
+ *
+ *  $Id: nfreader.c 48 2010-01-02 08:06:27Z haag $
+ *
+ *  $LastChangedRevision: 48 $
+ *
+ *
+ */
+
+/*
+ * nfreader is sample code for reading nfdump binary files.
+ * It accepts the standard nfdump file select options -r, -M and -R
+ * Therefore it allows you to loop over multiple files and process the netflow record.
+ *
+ * Insert your code in the process_data function after the call to ExpandRecord
+ * To build the binary: first compile nfdump as usual.
+ * Then compile nfreader:
+ *
+ * make nfreader
+ *
+ * This compiles this code and links the required nfdump files
+ * If you do it by hand:
+ *
+ * gcc -c nfreader.c
+ * gcc -o nfreader nfreader.o nffile.o flist.o util.o minilzo.o nfx.o libhiredis.a /usr/lib/libglib-2.0.a
+ *
+ */
+
+#include "config.h"
+
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdarg.h>
+#include <errno.h>
+#include <time.h>
+#include <string.h>
+#include <ctype.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+
+#ifdef HAVE_STDINT_H
+#include <stdint.h>
+#endif
+
+#include "nffile.h"
+#include "nfx.h"
+#include "util.h"
+#include "flist.h"
+#include "hiredis.h"
+#include "glib.h"
+#define BUFFSIZE 1048576
+#define MAX_BUFFER_SIZE 104857600
+
+struct tuple{
+    unsigned long long l1;
+    unsigned long long l2;
+} typedef TUPLE;
+
+
+#if ( SIZEOF_VOID_P == 8 )
+typedef uint64_t    pointer_addr_t;
+#else
+typedef uint32_t    pointer_addr_t;
+#endif
+
+// module limited globals
+extension_map_list_t extension_map_list;
+
+/* Function Prototypes */
+static void usage(char *name);
+
+static void index_record(void* record, char *s );
+void store_address(char* addr);
+
+static void process_data(void);
+
+/* Use global variables to avoid parameter passing */
+char *g_filename;
+unsigned long long g_index;
+redisContext* g_rctx;
+redisReply *reply;
+
+int rbidx;
+char dontcare = 0;
+GHashTable* ght;
+
+/* Functions */
+
+#include "nffile_inline.c"
+
+static void usage(char *name) {
+        printf("Create index in Redis describing the relation ship file  - IPaddress\n");
+		printf("usage %s [options] \n"
+					"-r\t\tread input from file\n"
+					, name);
+} /* usage */
+
+
+
+static void process_data(void) {
+data_block_header_t block_header;
+master_record_t		master_record;
+common_record_t     *flow_record, *in_buff;
+uint32_t	buffer_size;
+int 		i, rfd, done, ret;
+char		*string;
+#ifdef COMPAT15
+int	v1_map_done = 0;
+#endif
+
+	// Get the first file handle
+	rfd = GetNextFile(0, 0, 0, NULL);
+	if ( rfd < 0 ) {
+		if ( rfd == FILE_ERROR )
+			perror("Can't open input file for reading");
+		return;
+	}
+
+	// allocate buffer suitable for netflow version
+	buffer_size = BUFFSIZE;
+	in_buff = (common_record_t *) malloc(buffer_size);
+
+	if ( !in_buff ) {
+		perror("Memory allocation error");
+		close(rfd);
+		return;
+	}
+
+	done = 0;
+	while ( !done ) {
+		// get next data block from file
+		ret = ReadBlock(rfd, &block_header, (void *)in_buff, &string);
+
+		switch (ret) {
+			case NF_CORRUPT:
+			case NF_ERROR:
+				if ( ret == NF_CORRUPT )
+					fprintf(stderr, "Skip corrupt data file '%s': '%s'\n",GetCurrentFilename(), string);
+				else
+					fprintf(stderr, "Read error in file '%s': %s\n",GetCurrentFilename(), strerror(errno) );
+				// fall through - get next file in chain
+			case NF_EOF:
+				rfd = GetNextFile(rfd, 0, 0, NULL);
+				if ( rfd < 0 ) {
+					if ( rfd == NF_ERROR )
+						fprintf(stderr, "Read error in file '%s': %s\n",GetCurrentFilename(), strerror(errno) );
+
+					// rfd == EMPTY_LIST
+					done = 1;
+				} // else continue with next file
+				continue;
+
+				break; // not really needed
+		}
+
+#ifdef COMPAT15
+		if ( block_header.id == DATA_BLOCK_TYPE_1 ) {
+			common_record_v1_t *v1_record = (common_record_v1_t *)in_buff;
+			// create an extension map for v1 blocks
+			if ( v1_map_done == 0 ) {
+				extension_map_t *map = malloc(sizeof(extension_map_t) + 2 * sizeof(uint16_t) );
+				if ( ! map ) {
+					perror("Memory allocation error");
+					exit(255);
+				}
+				map->type 	= ExtensionMapType;
+				map->size 	= sizeof(extension_map_t) + 2 * sizeof(uint16_t);
+				map->map_id = 0;
+				map->ex_id[0]  = EX_IO_SNMP_2;
+				map->ex_id[1]  = EX_AS_2;
+				map->ex_id[2]  = 0;
+
+				Insert_Extension_Map(&extension_map_list, map);
+
+				v1_map_done = 1;
+			}
+
+			// convert the records to v2
+			for ( i=0; i < block_header.NumRecords; i++ ) {
+				common_record_t *v2_record = (common_record_t *)v1_record;
+				Convert_v1_to_v2((void *)v1_record);
+				// now we have a v2 record -> use size of v2_record->size
+				v1_record = (common_record_v1_t *)((pointer_addr_t)v1_record + v2_record->size);
+			}
+			block_header.id = DATA_BLOCK_TYPE_2;
+		}
+#endif
+
+		if ( block_header.id != DATA_BLOCK_TYPE_2 ) {
+			fprintf(stderr, "Can't process block type %u. Skip block.\n", block_header.id);
+			continue;
+		}
+
+		flow_record = in_buff;
+		for ( i=0; i < block_header.NumRecords; i++ ) {
+			char        string[1024];
+
+			if ( flow_record->type == CommonRecordType ) {
+				uint32_t map_id = flow_record->ext_map;
+				if ( extension_map_list.slot[map_id] == NULL ) {
+					snprintf(string, 1024, "Corrupt data file! No such extension map id: %u. Skip record", flow_record->ext_map );
+					string[1023] = '\0';
+				} else {
+					ExpandRecord_v2( flow_record, extension_map_list.slot[flow_record->ext_map], &master_record);
+
+					// update number of flows matching a given map
+					extension_map_list.slot[map_id]->ref_count++;
+
+                    index_record(&master_record, string);
+				}
+
+			} else if ( flow_record->type == ExtensionMapType ) {
+				extension_map_t *map = (extension_map_t *)flow_record;
+
+				if ( Insert_Extension_Map(&extension_map_list, map) ) {
+					 // flush new map
+				} // else map already known and flushed
+
+			} else {
+				fprintf(stderr, "Skip unknown record type %i\n", flow_record->type);
+			}
+
+			// Advance pointer by number of bytes for netflow record
+			flow_record = (common_record_t *)((pointer_addr_t)flow_record + flow_record->size);
+
+		} // for all records
+
+	} // while
+
+	if ( rfd > 0 )
+		close(rfd);
+
+	free((void *)in_buff);
+
+	PackExtensionMapList(&extension_map_list);
+
+} // End of process_data
+
+void connect_to_redisServer(char* hostname, char* port)
+{
+   struct timeval timeout = { 1, 500000 }; // 1.5 seconds
+   //FIXME Pass this information via command line
+   g_rctx = redisConnectWithTimeout((char*)"127.0.0.1", 6379, timeout);
+
+    if (g_rctx->err) {
+        printf("Connection error: %s\n", g_rctx->errstr);
+        exit(1);
+    }
+}
+
+/*FIXME NULL seems not to be returned on errors and errstr is not set? */
+
+void set_file_indexes(void)
+{
+
+    /* Get the next index value */
+    reply = redisCommand(g_rctx,"INCR c:fid");
+    if (!reply)
+        goto error;
+    g_index = reply->integer;
+    freeReplyObject(reply);
+
+    /* Set the file name index */
+    reply = redisCommand(g_rctx,"SET f:%s %d", g_filename, g_index);
+    if (!reply)
+        goto error;
+    freeReplyObject(reply);
+
+    /* Put the reverse index */
+    reply = redisCommand(g_rctx, "SET d:%d %s", g_index,g_filename);
+    if (!reply)
+        goto error;
+    freeReplyObject(reply);
+
+    return;
+
+error:
+    printf("Connection error: %s\n", g_rctx->errstr);
+    exit(1);
+}
+
+
+/*
+ * An address is composed of 128 including IPv4 and IPv6
+ * Most of the traffic is IPv4 resulting that l1 is almost always 0.
+ * Therefore, the l2 is returned to avoid linear scanning
+ */
+guint addrhash (gconstpointer key)
+{
+    TUPLE* t;
+    t = (TUPLE*)key;
+    guint out;
+    out = (guint)t->l2;
+    return out;
+}
+
+
+
+gboolean addreq(gconstpointer a, gconstpointer b)
+{
+    TUPLE *src;
+    TUPLE *dst;
+    src = (TUPLE*) a;
+    dst = (TUPLE*) b;
+    if ((src->l1 == dst->l1) && (src->l2 == dst->l2)){
+        return TRUE;
+    }
+    return FALSE;
+
+}
+
+
+inline int isInHashTable(TUPLE* key)
+{
+    if (g_hash_table_lookup(ght,key))
+        return 1;
+    return 0;
+}
+
+
+/*
+ * Put address in a local hash table in order to avoid redundant redis queries.
+ * On errors the addresses are not enqueued locally
+ */
+inline void enqueue(unsigned long long l1, unsigned long long l2)
+{
+    TUPLE* addr;
+    addr = malloc(sizeof(TUPLE));
+    if (addr){
+        addr->l1 = l1;
+        addr->l2 = l2;
+        g_hash_table_insert(ght,addr,&dontcare);
+        /* A char that is not used is used as value in the hash table */
+    }
+    /* If there is no memory it is simply not enqueued */
+}
+
+/*
+ * Check the local hash for an address encoded by l1 and l2.
+ * @Returns 0 if it is in the local cache
+ * @Returns 1 if it is in the local cache
+ */
+
+inline int checkqueue(unsigned long long l1, unsigned long long l2)
+{
+    TUPLE *addr;
+    addr = malloc(sizeof(TUPLE));
+    if (addr){
+        addr->l1 = l1;
+        addr->l2 = l2;
+        if (isInHashTable(addr)){
+            free(addr);
+            return 0;
+        }
+    }
+    /* FIXME Yep. The tuples in the hashtable are not freed except when the
+     *       at the end of the processing where the process is cleaned up by
+     *       the kernel
+     */
+
+    /* By default it is not found even if there is an error */
+    return 1;
+}
+
+void store_address(char* addr)
+{
+    reply = redisCommand(g_rctx,"SADD n%s %d", addr, g_index);
+   // printf("ADDR:%s\n",addr);
+    if (reply){
+          freeReplyObject(reply);
+    }else{
+        /* TODO syslog IP address and filename that could not indexed
+         *      Take care to not overwhelm the system (i.e. check and
+         *      compare error code)
+         */
+    fprintf(stderr,"Cannot store address: %s",addr);
+    }
+}
+
+/*
+ * Addresses are stored with their human readable representation
+ * in order to avoid parsing errors. An address starts with a
+ * nx:yyy
+ * x is either 4 or 6 (ipv4 or Ipv6)
+ * y is the string representation of the address
+ */
+
+static void index_record(void* record, char *s )
+{
+    master_record_t *r = (master_record_t *)record;
+    char srcaddr[128];
+    char dstaddr[128];
+    char buf[128];
+    unsigned long long int sl1;
+    unsigned long long int sl2;
+    unsigned long long int dl1;
+    unsigned long long int dl2;
+
+    if ( (r->flags & FLAG_IPV6_ADDR ) != 0 ) { // IPv6
+        r->v6.srcaddr[0] = htonll(r->v6.srcaddr[0]);
+        r->v6.srcaddr[1] = htonll(r->v6.srcaddr[1]);
+        r->v6.dstaddr[0] = htonll(r->v6.dstaddr[0]);
+        r->v6.dstaddr[1] = htonll(r->v6.dstaddr[1]);
+
+        inet_ntop(AF_INET6, r->v6.srcaddr, buf, sizeof(buf));
+        snprintf((char*) &srcaddr, 128, "6:%s",buf);
+
+        inet_ntop(AF_INET6, r->v6.dstaddr, buf, sizeof(buf));
+        snprintf((char*) &dstaddr, 128, "6:%s",buf);
+
+        sl1 = r->v6.srcaddr[0];
+        sl2 = r->v6.srcaddr[1];
+        dl1 = r->v6.dstaddr[0];
+        dl2 = r->v6.dstaddr[1];
+
+    } else { //IPv4
+        r->v4.srcaddr = htonl(r->v4.srcaddr);
+        r->v4.dstaddr = htonl(r->v4.dstaddr);
+
+        inet_ntop(AF_INET, &r->v4.srcaddr, buf, sizeof(buf));
+        snprintf((char*) &srcaddr, 128, "4:%s",buf);
+
+        inet_ntop(AF_INET, &r->v4.dstaddr, buf, sizeof(buf));
+        snprintf((char*) &dstaddr, 128, "4:%s",buf);
+
+        sl1 = 0;
+        sl2 = r->v4.srcaddr;
+        dl1 = 0;
+        dl2 = r->v4.dstaddr;
+    }
+
+    /* Store address in redis if needed */
+    if (checkqueue(sl1,sl2))
+        store_address(srcaddr);
+    enqueue(sl1,sl2);
+    if (checkqueue(dl1,dl2))
+        store_address(dstaddr);
+    enqueue(dl1,dl2);
+}/* index record */
+
+
+int main( int argc, char **argv ) {
+char 		*rfile, *Rfile, *Mdirs;
+int			c;
+    // TODO update command line
+	rfile = Rfile = Mdirs = NULL;
+	while ((c = getopt(argc, argv, "L:r:M:R:")) != EOF) {
+		switch (c) {
+			case 'h':
+				usage(argv[0]);
+				exit(0);
+				break;
+				break;
+			case 'L':
+				if ( !InitLog("argv[0]", optarg) )
+					exit(255);
+				break;
+			case 'r':
+				rfile = optarg;
+				if ( strcmp(rfile, "-") == 0 )
+					rfile = NULL;
+				break;
+			case 'M':
+				Mdirs = optarg;
+				break;
+			case 'R':
+				Rfile = optarg;
+				break;
+			default:
+				usage(argv[0]);
+				exit(0);
+		}
+	}
+
+	if ( rfile && Rfile ) {
+		fprintf(stderr, "-r and -R are mutually exclusive. Please specify either -r or -R\n");
+		exit(255);
+	}
+	if ( Mdirs && !(rfile || Rfile) ) {
+		fprintf(stderr, "-M needs either -r or -R to specify the file or file list. Add '-R .' for all files in the directories.\n");
+		exit(255);
+	}
+
+    ght = g_hash_table_new(addrhash, addreq);
+    /* Make filename global to accessing in the indexing function */
+    g_filename = rfile;
+    /* Connect to redis server  and set file indexes*/
+    connect_to_redisServer(NULL,0);
+    set_file_indexes();
+
+	InitExtensionMaps(&extension_map_list);
+
+	SetupInputFileSequence(Mdirs, rfile, Rfile);
+
+	process_data();
+
+
+	return 0;
+}
