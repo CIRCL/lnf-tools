a9740e7604faea456d45374b38d7d4b87ae67c9d
Gerard Wagener <gerard.wagener@circl.lu>
2011-11-30 17:41:35 +0100

    klookup can focus on a delimited time interval

--- bin/klookup.py
+++ bin/klookup.py
@@ -17,6 +17,10 @@
 #
 # You should have received a copy of the GNU General Public License
 # along with this program. If not, see <http://www.gnu.org/licenses/>.
+#
+#TODO Add sorting option i.e. reverse sorting
+#TODO implement pcap filter here aswell
+#TODO There should not be any missing databases. Maybe the closed database could be searched
 import os
 import sys
 from kyotocabinet import *
@@ -24,6 +28,7 @@ import getopt
 import time
 import kindcommon
 import ConfigParser
+import unittest
 
 class Klookup(object):
 
@@ -94,18 +99,97 @@ quotation marks.
 """
         sys.exit(exitcode)
 
-    def get_databases_list(self):
+    def get_file_position(self, files, date, default):
+        if date == None:
+            #No date was specified return default value
+            return default
+        if self.check_date_str(date) == False:
+            self.kco.dbg('Invalid date string was provided '+ date + ' reset to None')
+            return default
+        date = date.replace('-','/')
+        date = date + '.kch'
+        self.kco.dbg('Converted date string: ' + date)
+        #Go through all elements and search the date
+        i = 0
+        for f in files:
+            if f == date:
+                self.kco.dbg('Found date '+ date + 'at position '+ str(i))
+                return i
+            i = i + 1
+        self.kco.dbg('The index of the file ' + date + 'was not found returning ' + str(default) )
+        return default
+
+
+    def check_date_str(self,datestr):
+        try:
+            (year, month, day)  = datestr.split('-')
+            int(year)
+            int(month)
+            int(day)
+            if len(year) != 4:
+                return False
+            if len(month) != 2:
+                return False
+            if len(day) != 2:
+                return False
+
+            #If still alive here it should be good
+            return True
+        except ValueError,e:
+            self.kco.dbg('check_date_str value error '+ str(e))
+            return False
+        #This should never be executed
+        return False
+
+    #If date strings are not valid the entire database is selected
+    def get_relevant_files(self, files,startdate, enddate):
+        #Strip the root prefix
+        dbdir = self.config.get('indexer','dbdir')
+        if dbdir.endswith('/')==False:
+            dbdir = dbdir + '/'
+        sfiles = []
+        for f in files:
+            f = f.replace(dbdir,'')
+            sfiles.append(f)
+        #The ascii sort should sort it accordingly time
+        #The year is first hence the lowest year should be first
+        #The month is next hence after the sorting of year the months
+        #are sorted
+        #idem for the days
+        sfiles.sort()
+        fl = len(sfiles)
+        if fl == 0:
+            return []
+        startpos = self.get_file_position(sfiles, startdate, 0)
+        endpos = self.get_file_position(sfiles, enddate, fl)
+        self.kco.dbg('get_relevant_files: startdate ' + str(startdate))
+        self.kco.dbg('get_relevant_files: enddate ' + str(enddate))
+        self.kco.dbg('get_relevant_files: fl ' + str(fl) + '\n')
+        self.kco.dbg('get_relevant_files: startpos=' +  str(startpos))
+        self.kco.dbg('get_relevant_files: endpos=' +  str(endpos))
+        #The : notation should not throw an exception
+        stfiles = sfiles[startpos:endpos+1]
+        #Add prefix to the files
+        files= []
+        for f in stfiles:
+            #The slash has already be added to dbdir
+            files.append(dbdir + f)
+        return files
+
+    def get_databases_list(self, startdate=None, enddate=None):
         dbdir  = self.config.get('indexer','dbdir')
         files = []
         for dirname, dirnames, filenames in os.walk(dbdir):
             for filename in filenames:
-                files.append(os.path.join(dirname, filename))
-        return files
+                afile = os.path.join(dirname, filename)
+                files.append(afile)
+        rfiles = self.get_relevant_files(files, startdate, enddate)
+        return rfiles
 
 
-    def open_databases(self):
+    def open_databases(self, startdate=None,enddate=None):
         self.dbobjs = []
-        for i in self.get_databases_list():
+        for i in self.get_databases_list(startdate, enddate):
             db = DB()
             if not db.open(i, DB.OREADER ):
                 print >>sys.stderr, "open error: " + str(db.error())
@@ -126,9 +210,9 @@ quotation marks.
 
 
 
-    def get_filenames(self):
+    def get_filenames(self,startdate, enddate):
         dbdir = self.config.get('indexer','dbdir')
-        self.open_databases()
+        self.open_databases(startdate, enddate)
         files = []
         ky = self.kco.build_key(self.ipaddress)
         for db in self.dbobjs:
@@ -141,17 +225,17 @@ quotation marks.
                     files.append(afn)
         return files
 
-    def print_filenames(self):
-        files = self.get_filenames()
+    def print_filenames(self, startdate, enddate):
+        files = self.get_filenames(startdate, enddate)
         for f in files:
             print self.ipaddress," ",f
 
-    def print_rel_filenames (self):
-        files = self.get_filenames()
+    def print_rel_filenames (self, startdate, enddate):
+        files = self.get_filenames(startdate, enddate)
         for f in files:
             print self.ipaddress," ",os.path.basename(f)
 
-    def getfull_flows(self):
+    def getfull_flows(self, startdate, enddate):
         try:
             prg = self.config.get("nfdump","prg")
             args = self.config.get("nfdump", "args")
@@ -159,7 +243,7 @@ quotation marks.
         except ConfigParser.NoSectionError,e:
             sys.stderr.write(str(e)+ "\n")
             sys.exit(1)
-        self.open_databases()
+        self.open_databases(startdate, enddate)
 
         ky = self.kco.build_key(self.ipaddress)
         for db in self.dbobjs:
@@ -191,15 +275,15 @@ quotation marks.
                 return True
 
 def main_function():
-    #### main function
-
     ipaddress=None
     format=None
+    startts = None
+    endts   = None
     kl = Klookup()
 
     try:
         #Parse command line arguments
-        opts, args = getopt.getopt(sys.argv[1:],'hf:i:c:')
+        opts, args = getopt.getopt(sys.argv[1:],'hf:i:c:s:e:')
         for o,a in opts:
             if o == '-h':
                 kl.usage(0)
@@ -209,6 +293,11 @@ def main_function():
                 kl.configFile = a
             elif o =='-f':
                 format = a
+            elif o == '-s':
+                startts = a
+            elif o == '-e':
+                endts = a
+
         if ipaddress == None:
             sys.stderr.write('An IP address or a list of IP addresses must be specified\n')
             sys.exit(1)
@@ -224,11 +313,11 @@ def main_function():
                     sys.exit(1)
 
             if format.startswith('print relative'):
-                kl.print_rel_filenames();
+                kl.print_rel_filenames(startts, endts);
                 sys.exit(0)
 
             if format.startswith('print full'):
-                kl.getfull_flows()
+                kl.getfull_flows(startts, endts)
                 sys.exit(0)
 
         #Here some printing is done
@@ -238,7 +327,7 @@ def main_function():
 
         #default format
         startdate=time.time()
-        kl.print_filenames()
+        kl.print_filenames(startts,endts)
     except getopt.GetoptError,e:
         sys.stderr.write(str(e)+ '\n')
         sys.exit(1)
@@ -247,5 +336,38 @@ def main_function():
     print "#Processing time: ",d
     sys.exit(0)
 
+class TestDatabases(unittest.TestCase):
+    def testFiles(self):
+        kl = Klookup()
+        kl.configFile = '../t/klookup/kindexer.cfg'
+        kl.load()
+        #Test simple case
+        lst = kl.get_databases_list('2011-10-05', '2011-10-07')
+        self.assertEqual(lst, ['../t/klookup/databases/2011/10/05.kch', '../t/klookup/databases/2011/10/06.kch', '../t/klookup/databases/2011/10/07.kch'])
+
+        #Test month flip
+        lst = kl.get_databases_list('2011-10-30','2011-11-01')
+        self.assertEqual(lst, ['../t/klookup/databases/2011/10/30.kch', '../t/klookup/databases/2011/10/31.kch', '../t/klookup/databases/2011/11/01.kch'])
+
+        #Test unknown start date
+        lst = kl.get_databases_list(None,'2011-10-02')
+        self.assertEqual(lst, ['../t/klookup/databases/2011/10/01.kch', '../t/klookup/databases/2011/10/02.kch'])
+
+        #Test unknown end date
+        lst = kl.get_databases_list('2011-11-29',None)
+        self.assertEqual(lst, ['../t/klookup/databases/2011/11/29.kch', '../t/klookup/databases/2011/11/30.kch', '../t/klookup/databases/2011/11/31.kch'])
+
+        #Test start date not found
+        lst = kl.get_databases_list('aaa')
+        res = ['../t/klookup/databases/2011/10/01.kch', '../t/klookup/databases/2011/10/02.kch', '../t/klookup/databases/2011/10/03.kch', '../t/klookup/databases/2011/10/04.kch', '../t/klookup/databases/2011/10/05.kch', '../t/klookup/databases/2011/10/06.kch', '../t/klookup/databases/2011/10/07.kch', '../t/klookup/databases/2011/10/08.kch', '../t/klookup/databases/2011/10/09.kch', '../t/klookup/databases/2011/10/10.kch', '../t/klookup/databases/2011/10/11.kch', '../t/klookup/databases/2011/10/12.kch', '../t/klookup/databases/2011/10/13.kch', '../t/klookup/databases/2011/10/14.kch', '../t/klookup/databases/2011/10/15.kch', '../t/klookup/databases/2011/10/16.kch', '../t/klookup/databases/2011/10/17.kch', '../t/klookup/databases/2011/10/18.kch', '../t/klookup/databases/2011/10/19.kch', '../t/klookup/databases/2011/10/20.kch', '../t/klookup/databases/2011/10/21.kch', '../t/klookup/databases/2011/10/22.kch', '../t/klookup/databases/2011/10/23.kch', '../t/klookup/databases/2011/10/24.kch', '../t/klookup/databases/2011/10/25.kch', '../t/klookup/databases/2011/10/26.kch', '../t/klookup/databases/2011/10/27.kch', '../t/klookup/databases/2011/10/28.kch', '../t/klookup/databases/2011/10/29.kch', '../t/klookup/databases/2011/10/30.kch', '../t/klookup/databases/2011/10/31.kch', '../t/klookup/databases/2011/11/01.kch', '../t/klookup/databases/2011/11/02.kch', '../t/klookup/databases/2011/11/03.kch', '../t/klookup/databases/2011/11/04.kch', '../t/klookup/databases/2011/11/05.kch', '../t/klookup/databases/2011/11/06.kch', '../t/klookup/databases/2011/11/07.kch', '../t/klookup/databases/2011/11/08.kch', '../t/klookup/databases/2011/11/09.kch', '../t/klookup/databases/2011/11/10.kch', '../t/klookup/databases/2011/11/11.kch', '../t/klookup/databases/2011/11/12.kch', '../t/klookup/databases/2011/11/13.kch', '../t/klookup/databases/2011/11/14.kch', '../t/klookup/databases/2011/11/15.kch', '../t/klookup/databases/2011/11/16.kch', '../t/klookup/databases/2011/11/17.kch', '../t/klookup/databases/2011/11/18.kch', '../t/klookup/databases/2011/11/19.kch', '../t/klookup/databases/2011/11/20.kch', '../t/klookup/databases/2011/11/21.kch', '../t/klookup/databases/2011/11/22.kch', '../t/klookup/databases/2011/11/23.kch', '../t/klookup/databases/2011/11/24.kch', '../t/klookup/databases/2011/11/25.kch', '../t/klookup/databases/2011/11/26.kch', '../t/klookup/databases/2011/11/27.kch', '../t/klookup/databases/2011/11/28.kch', '../t/klookup/databases/2011/11/29.kch', '../t/klookup/databases/2011/11/30.kch', '../t/klookup/databases/2011/11/31.kch']
+        self.assertEqual(lst,res)
+
+        #Test no time stamps
+        lst = kl.get_databases_list(None, None)
+        self.assertEqual(lst, res)
+
+
 if __name__ == '__main__':
+    #unittest.main()
     main_function()
+
